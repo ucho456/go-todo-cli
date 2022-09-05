a307f2746168ce2626a9340f5abd7fcfbe275a4a
ucho456 <watapqxyz@gmail.com>
2022-09-05 17:16:28 +0900

    Add todo with input

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"flag"
 	"fmt"
+	"io"
 	"os"
+	"strings"
 
 	"github.com/ucho456/go_todo_cli/todo"
 )
@@ -26,10 +30,35 @@ func main() {
 
 	switch {
 	case *add:
-		todos.Add("Sample todo")
+		task, err := getInput(os.Stdin, flag.Args()...)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+		todos.Add(task)
 		if err := todos.Store(todoFile); err != nil {
 			fmt.Fprintln(os.Stderr, err.Error())
 			os.Exit(1)
 		}
 	}
+}
+
+func getInput(r io.Reader, args ...string) (string, error) {
+	if len(args) > 0 {
+		return strings.Join(args, " "), nil
+	}
+
+	scanner := bufio.NewScanner(r)
+	scanner.Scan()
+	if err := scanner.Err(); err != nil {
+		return "", err
+	}
+
+	text := scanner.Text()
+
+	if len(text) == 0 {
+		return "", errors.New("empty todo is not allowed")
+	}
+
+	return text, nil
 }
